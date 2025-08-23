50594cef5dbd77ee6d691f5271e5656f470cde7c
Darthsae <sockmit2007@gmail.com>
2025-08-23 14:40:56 -0400

    DLL.

--- TeraTomaLibrary/include/TeraToma/DLLExports.h
+++ TeraTomaLibrary/include/TeraToma/DLLExports.h
@@ -1,9 +1,17 @@
 #pragma once
 #ifndef DLL_EXPORTS_H
 #define DLL_EXPORTS_H
-#ifdef DLL_EXPORTS
-    #define MOD_API __declspec(dllexport)
-#else
-    #define MOD_API __declspec(dllimport)
+#if defined(_WIN32) || defined(_WIN64)
+    #ifdef DLL_EXPORTS
+        #define MOD_API __declspec(dllexport)
+    #else
+        #define MOD_API __declspec(dllimport)
+    #endif
+#endif
+#if defined(__linux__) || defined(__unix__) || defined(linux)
+    #define MOD_API
+#endif
+#if defined(__APPLE__)
+    #define MOD_API
 #endif
 #endif
